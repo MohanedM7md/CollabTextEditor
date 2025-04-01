9702aa0cb2b206db292977c6db9994004f87fea4
Mohaned <mohandmohamed104@gmail.com>
2025-04-01 16:55:38 +0200

    intialize

---
+++ Server/src/main/java/com/editor/collabtexteditor/CollabTextEditorApplication.java
@@ -0,0 +1,13 @@
+package com.editor.collabtexteditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabTextEditorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollabTextEditorApplication.class, args);
+    }
+
+}
---
+++ Server/src/test/java/com/editor/collabtexteditor/CollabTextEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.editor.collabtexteditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabTextEditorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
